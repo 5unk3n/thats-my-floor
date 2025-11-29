e3fae1c3460ad752c384f3588c1f0975de47c005
Seongeun Lee <tjsrms1227@gmail.com>
2025-11-29 22:46:57 +0900

    ✨ Feat : ConcertDetail 컴포넌트 구현

---
+++ src/features/concerts/components/ConcertDetail.tsx
@@ -0,0 +1,18 @@
+import { KopisConcertDetailResponse } from '@/shared/lib/kopis/types';
+import { ConcertInfo } from './ConcertInfo';
+import { ConcertImages } from './ConcertImages';
+
+type ConcertDetailData = KopisConcertDetailResponse['dbs']['db'];
+
+interface ConcertDetailProps {
+  concert: ConcertDetailData;
+}
+
+export function ConcertDetail({ concert }: ConcertDetailProps) {
+  return (
+    <div className="container mx-auto px-4 py-8 space-y-8 max-w-5xl">
+      <ConcertInfo concert={concert} />
+      <ConcertImages concert={concert} />
+    </div>
+  );
+}
