2f4df011e27db97f0a0889a75e5798fe8476718d
Seongeun Lee <tjsrms1227@gmail.com>
2026-01-14 23:16:50 +0900

    🔨 Modify: manifest 수정
    - 이름 변경
    - id 추가
    - 아이콘 추가

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,11 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "That's My Floor - 콘서트 정보 알리미",
-    short_name: "That's My Floor",
+    name: "That's My Floor",
+    short_name: 'TMF',
     description: '좋아하는 아티스트의 공연 정보를 놓치지 마세요',
+    lang: 'ko-KR',
+    id: '/',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
@@ -14,11 +16,25 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/android/android-launchericon-192-192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/android/android-launchericon-192-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/android/android-launchericon-512-512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/android/android-launchericon-512-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
