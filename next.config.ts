e3454fec1deab46ed6bb2d8bbcb44e85934ea26d
Seongeun Lee <tjsrms1227@gmail.com>
2026-01-15 00:24:04 +0900

    🐛 Fix : 번들에 증분 업데이트용 패키지 포함

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [new URL('http://www.kopis.or.kr/**'), new URL('https://i.scdn.co/image/**')],
   },
   cacheComponents: true,
+  serverExternalPackages: ['tar-stream', 'unbzip2-stream'], // 증분 업데이트 스크립트용
 };
 
 export default withSerwist(nextConfig);
